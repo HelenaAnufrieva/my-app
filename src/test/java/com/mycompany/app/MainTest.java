94cb65b5b7639c50030bebbcd76e22cbf4a8608d
HelenaAnufrieva <elen.nuf@gmail.com>
2017-07-13 20:47:59 +0300

    Update MainTest.java

--- src/test/java/com/mycompany/app/MainTest.java
+++ src/test/java/com/mycompany/app/MainTest.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -12,15 +14,22 @@ public class MainTest {
     Sum sum;
 
     @Before
-    public void setUp() throws Exception
-    {
+    public void setUp() throws Exception {
         sum = new Sum();
+        System.out.println("SetUp");
+    }
+
+    @BeforeClass
+    public static void setUp1() throws Exception
+    {
+        System.out.println("SetUp1");
     }
 
     @Test
-    public void testPlus() throws Exception
-    {
-        assertEquals(4, sum.plus(2,2));
+    public void testPlus() throws Exception {
+        assertEquals(4, sum.plus(2, 2));
+        System.out.println("Test");
     }
+
 
 }
